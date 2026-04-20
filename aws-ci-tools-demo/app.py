86426f83564f66a1b8f75196a5d59668de1d5e1d
Mustapha Barrie <mbarrie07@gmail.com>
2026-04-19 23:17:55 -0400

    Implemented flask app

---
+++ aws-ci-tools-demo/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to my DevOps Journey"
+
+@app.route('/about')
+def about():
+    return "Mustapha is a fast, curious learner with a can do attitude. In pursuit of being a cracked Engineer!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
