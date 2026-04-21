b3fe0552e4b8156ba1437dff7ce58b654d0899ec
Mustapha Barrie <mbarrie07@gmail.com>
2026-04-21 17:38:02 -0400

    Add new route

--- aws-ci-tools-demo/app.py
+++ aws-ci-tools-demo/app.py
@@ -10,5 +10,10 @@ def home():
 def about():
     return "Mustapha is a fast, curious learner with a can do attitude. In pursuit of being a cracked Engineer!"
 
+@app.route('/DevOps')
+def devops():
+    return "DevOps is a set of practices that combines software development (Dev) and IT operations (Ops) to shorten the development lifecycle and deliver software faster."
+
+
 if __name__ == '__main__':
     app.run(debug=True)
